e12356da16a5c1a0470ec493ae8ee6d8719d93ff
renanb12 <renanbernardo315@gmail.com>
2026-06-01 23:33:04 -0300

    Classe Prato, finalizada

--- src/Prato.java
+++ src/Prato.java
@@ -1,15 +1,14 @@
-import java.util.Scanner;
-
 public class Prato {
     private String nome;
     private double valor;
-    private Ingrediente vetIngredientes[] = new Ingrediente[10];
+    private Ingrediente vetIngredientes[];
     private int index;
 
-    public Prato(String nome, double valor, Ingrediente[] vetIngredientes, int index) {
+    public Prato(String nome, double valor, int quantidadeIngredientes) {
         this.nome = nome;
         this.valor = valor;
-        this.vetIngredientes = vetIngredientes;
+        this.vetIngredientes = new Ingrediente[quantidadeIngredientes];
+        this.index = 0;
     }
 
     public String getNome() {
@@ -44,15 +43,62 @@ public class Prato {
         this.index = index;
     }
 
-    public void adicionarIngrediente(Ingrediente ingredientes[]){
+    public boolean adicionarIngrediente(Ingrediente ingrediente){
         if (index < this.vetIngredientes.length){
-            Scanner input = new Scanner(System.in);
-            System.out.println("Digite o nome do ingrediente: ");
-            String nome = input.nextLine();
-
-            System.out.println("Digite ");
+            vetIngredientes[index] = ingrediente;
+            index++;
+            return true;
         } else {
-            System.out.println("Número máximo de ingredientes adicionado. ");
+            return false;
         }
+    }
+
+    public int buscarPosicao(String nomeIngrediente){
+        for (int i = 0; i < index; i++) {
+            if (vetIngredientes[i].getNome().equalsIgnoreCase(nomeIngrediente)){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public Ingrediente consultarIngrediente(String nomeIngrediente){
+        int posicao = buscarPosicao(nomeIngrediente);
+
+        if (posicao >= 0){
+            return vetIngredientes[posicao];
+        }
+
+        return null;
+    }
+
+    public boolean removerIngrediente(String nomeIngrediente){
+        int posicao = buscarPosicao(nomeIngrediente);
+
+        if (posicao >= 0){
+            for (int i = posicao; i < index - 1; i++) {
+                vetIngredientes[i] = vetIngredientes[i + 1];
+            }
+            vetIngredientes[index - 1] = null;
+            index--;
+            return true;
+        }
+
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        String retorno = "";
+
+        retorno += "Nome: " + nome + "\n";
+        retorno += "Valor: R$ " + valor + "\n";
+        retorno += "Ingredientes:\n";
+
+        for (int i = 0; i < index; i++) {
+            retorno += vetIngredientes[i] + "\n";
+        }
+
+        return retorno;
     }
 }
